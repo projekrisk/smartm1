72f66b77dba4d488b0531a579a38195ccaae5574
Projekrisk <projekrisk@gmail.com>
2026-08-09 22:33:06 +0700

    tabel dispen

--- database/migrations/2026_08_06_191445_create_surat_dispensasis_table.php
+++ database/migrations/2026_08_06_191445_create_surat_dispensasis_table.php
@@ -6,35 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up()
+    public function up(): void
     {
         Schema::create('siswa_surat_dispensasi', function (Blueprint $table) {
             $table->id();
             
-            $table->unsignedBigInteger('surat_dispensasi_id');
-            
-            $table->unsignedBigInteger('siswa_id'); 
-
-            $table->foreign('surat_dispensasi_id')
-                ->references('id')
-                ->on('surat_dispensasi')
-                ->onDelete('cascade');
-                
-            $table->foreign('siswa_id')
-                ->references('id')
-                ->on('siswa')
-                ->onDelete('cascade');
+            $table->foreignId('surat_dispensasi_id')
+                  ->constrained('surat_dispensasi')
+                  ->cascadeOnDelete();
+                  
+            $table->foreignId('siswa_id')
+                  ->constrained('siswa')
+                  ->cascadeOnDelete();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('surat_dispensasis');
+        Schema::dropIfExists('siswa_surat_dispensasi');
     }
 };
